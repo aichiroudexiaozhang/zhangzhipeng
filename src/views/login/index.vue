18dc9390cb05f0bfe7fef3fadf87f575cf3243e8
zhangx <18875723023@163.com>
2019-06-22 20:39:32 +0800

    样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <p>login.vue</p>
+  <div class="box">
+    <div class="login-img">
+      <img src="./logo_index.png" alt="">
+    </div>
+  <div class="login-werp">
+<el-form ref="form" :model="form" label-width="80px">
+  <el-form-item>
+    <el-input v-model="form.mobile" placeholder="手机号"></el-input>
+  </el-form-item>
+   <el-form-item>
+     <el-col :span="10"> 
+    <el-input v-model="form.code "  placeholder="验证码"></el-input>       
+     </el-col>
+     <el-col :span="10" :offset="2"> 
+     <el-button round>发送验证码</el-button>       
+     </el-col>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">登陆</el-button>
+  </el-form-item>
+</el-form>
+  </div>
   </div>
 </template>
 <script>
@@ -8,11 +28,27 @@ export default {
   name: 'AppLogin',
   data () {
     return {
-
+    form: {
+        mobile: '',
+        code: '',
+        }
     }
-  }
+  },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
 }
 </script>
 
 <style lang='less'>
+  .box{
+    margin: 200px auto;
+    width: 400px;
+    text-align: center;
+    padding: 20px;
+    background-color: aqua;
+  }
+
 </style>
